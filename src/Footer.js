3a87cac18a997041871dcb2ea2e1938d7aed9fe3
jana-droidcon <154016511+jana-droidcon@users.noreply.github.com>
2024-03-23 15:10:47 +0100

    Create repository, dependencies, css and js

---
+++ src/Footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <p>
+        This project was coded by
+        <a
+          href="https://github.com/jana-droidcon"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Jana Hille
+        </a>
+        , is on
+        <a
+          href="https://github.com/jana-droidcon/React-week-4"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Github
+        </a>
+        , hosted on
+        <a
+          href="https://weathersearchengine-janahille.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Netlify
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
